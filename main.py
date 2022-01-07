94e1010acfcd0319e610a3fb4257e80cf7d9f238
Broque-web <96385089+Broque-web@users.noreply.github.com>
2022-01-07 15:31:24 -0800

    added real time currency conversion and referral

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-#------------------------------------------------------
+# ------------------------------------------------------
 # MODULES IMPORTS
 from re import X
 import string
@@ -15,22 +15,25 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import backref, defaultload
 from flask.sessions import SecureCookieSessionInterface
 from werkzeug.datastructures import Headers
+from werkzeug.utils import secure_filename
 from flask_cors import CORS, cross_origin
+from static.src.Currency_converter.pycurrency import Currency_Converter
 
-#------------------------------------------------------
-#NECESSARY CONFIGURATIONS
-
+# ------------------------------------------------------
+# NECESSARY CONFIGURATIONS
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "b423653a944928b2e59984be767eefa5"
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///broke.db"
 app.config['CORS_HEADERS'] = 'Content-Type'
+app.config["UPLOAD_FOLDER"] = "static/images/slip_images/"
+allowed_extensions = {'jpg', 'png', 'jpeg'}
 db = SQLAlchemy(app)
 CORS(app)
 session_cookie = SecureCookieSessionInterface().get_signing_serializer(app)
 
 
-#------------------------------------------------------
-#DATABASE MODELS
+# ------------------------------------------------------
+# DATABASE MODELS
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -40,8 +43,14 @@ class User(db.Model):
     password = db.Column(db.String(60), nullable=False)
     country = db.Column(db.Integer)
     gender = db.Column(db.String())
+    referral_ref = db.Column(db.String())
+    balance = db.Column(db.Float)
     verified = db.Column(db.String(3), nullable=False, default="no")
     image = db.Column(db.String(20), nullable=False, default="default.png")
+    email = db.Column(db.String(), default="")
+    bio = db.Column(db.String(), default="")
+    DOB = db.Column(db.String(), default="")
+    phone = db.Column(db.String(), default="")
 
     def __repr__(self):
         return f"User('{self.username}', '{self.id}', '{self.image}')"
@@ -60,17 +69,22 @@ class Slip(db.Model):
     slip_ref = db.Column(db.String(10))
     likes = db.Column(db.Integer, default=0)
     bkl = db.Column(db.String())
-    date = db.Column(db.String(), default=datetime.utcnow().strftime("%d %b, %Y"))
+    image = db.Column(db.String())
+    date = db.Column(
+        db.String(), default=datetime.utcnow().strftime("%d %b, %Y"))
     expired = db.Column(db.String(), default="Not expired")
+
 
 class Blog(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    ref  = db.Column(db.String(10))
+    ref = db.Column(db.String(10))
     content = db.Column(db.String())
     title = db.Column(db.String())
     blog_owner = db.Column(db.Integer)
     blog_quote = db.Column(db.String(10))
-    date = db.Column(db.String(), default=datetime.utcnow().strftime("%d %b, %Y"))
+    date = db.Column(
+        db.String(), default=datetime.utcnow().strftime("%d %b, %Y"))
+
 
 class SlipComment(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -79,49 +93,77 @@ class SlipComment(db.Model):
     slip_id = db.Column(db.Integer)
     commenter_name = db.Column(db.String())
 
+
 class SlipLikes(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     Liker = db.Column(db.Integer)
     slip_id = db.Column(db.Integer)
     like = db.Column(db.Integer)
-#------------------------------------------------------
-#OTHER CONFIGURATIONS
+
+
+# ------------------------------------------------------
+# OTHER CONFIGURATIONS
 if os.path.exists("./broke.db"):
     pass
 else:
     db.create_all()
 
-#------------------------------------------------------
-#OTHER USEFUL FUNCTIONS
+# ------------------------------------------------------
+# OTHER USEFUL FUNCTIONS
+
+
+def get_con_balance(country):
+    try:
+        if country == 'Ghana':
+            return float(Currency_Converter().convert("USD", "GHS", 0.1)['result'].split(" ")[1])
+        elif country == 'Nigeria':
+            return float(Currency_Converter().convert("USD", "NGN", 0.1)['result'].split(" ")[1])
+        elif country == 'Kenya':
+            return float(Currency_Converter().convert("USD", "KES", 0.1)['result'].split(" ")[1])
+        else:
+            return 0.00
+    except:
+        return 0.0
+
+
 def generate_link(bookmaker, code):
     link = ''
     if bookmaker.lower() == "sportybet":
         link = f'https://sportybet.com/?shareCode={code}'
 
     elif bookmaker.lower() == '1xbet':
-        link = f'http://www.betway.com/bookabet/{code}'
-    elif bookmaker.lower() == 'betway':
         link = ''
-    return  link
+    elif bookmaker.lower() == 'betway':
+        link = f'http://www.betway.com/bookabet/{code}'
+    return link
+
+
 def add_session(username):
     if 'username' in session:
         pass
     else:
         session['username'] = username
 
+
+def allowed_file(filename):
+    return '.' in filename and \
+        filename.rsplit('.', 1)[1].lower() in allowed_extensions
+
+
 def expired():
     slips = Slip.query.all()
     for slip in slips:
         i_time = slip.time
         f_time = datetime.now().replace(microsecond=0, second=0)
-        time = i_time-f_time
+        time = i_time - f_time
         if str(time).startswith("-"):
             slip.expired = "Expired"
-        elif int(str(time).split(":")[0]) == 0 and int(str(time).split(":")[1]) <= 5:
+        elif int(str(time).split(":")[0]) == 0 and int(str(time).split(":")[1]) <= 2:
             slip.expired = "Expired"
         else:
             slip.expired = "Not expired"
     db.session.commit()
+
 
 def get_currency(country):
     THIS_FOLDER = os.path.dirname(os.path.abspath(__file__))
@@ -134,7 +176,7 @@ def get_currency(country):
     code = ""
     for countries in c_udt:
         if countries["name"] == country:
-            code =  countries["code"]
+            code = countries["code"]
             break
 
     currency = ""
@@ -142,15 +184,18 @@ def get_currency(country):
         if codes["country"] == code:
             currency = codes["currency"]
             break
-    #print(code)
+    # print(code)
     return currency
 
 
 def hash(password):
     hash = hashlib.sha256(password.encode("utf-8")).hexdigest()
     return hash
+
+
 def delete_session():
     session.pop("username", None)
+
 
 def is_logged_in():
     if 'username' in session:
@@ -168,42 +213,70 @@ def generate_ref(size):
     return ref
 
 
-
-#------------------------------------------------------
-#ROUTES
+# ------------------------------------------------------
+# ROUTES
 @app.before_request
 def before_request():
     expired()
-    if 'currency' in session and 'username'  in session:
-        pass
+    if 'currency' in session and 'username' in session:
+        user = User.query.filter_by(username=session['username']).first()
+        session['balance'] = format(user.balance, '.2f')
+        session['image'] = user.image
     else:
         session['currency'] = "USD"
+
 
 @app.route("/")
 def home():
     home_slips = Slip.query.filter_by(expired="Not expired").all()[0:7]
     blogs = Blog.query.all()[0:2]
+    if request.args.get('ref'):
+        print(request.args.get('ref'))
+        session['ref'] = request.args.get('ref')
+        return redirect(url_for('home'))
     return render_template("index.html", home_slips=home_slips, blogs=blogs)
+
+
+@app.route("/referral")
+def referral():
+    if not is_logged_in():
+        flash("Please login first", "danger")
+        session['redirect'] = request.url
+        return redirect(url_for("signin"))
+    user = User.query.filter_by(username=session['username']).first()
+    try:
+        ref_people = int((User.query.filter_by(username=session['username']).first(
+        ).balance / get_con_balance(User.query.filter_by(username=session['username']).first().country)) - 1)
+    except:
+        ref_people = "-"
+    return render_template("referral.html", ref=request.root_url+'?ref='+user.referral_ref, ref_people=ref_people)
+
 
 @app.route("/about")
 def about():
-    return render_template("about.html")
-    
+    users = len(User.query.all())
+    slips = len(Slip.query.filter_by(expired="Not expired").all())
+    return render_template("about.html", users=users, slips=slips)
+
+
 @app.route("/blog-sub", methods=["POST", "GET"])
 def blogSub():
     if not is_logged_in():
         flash("Please login first", "danger")
+        session['redirect'] = request.url
         return redirect(url_for("signin"))
     elif request.method == "POST":
         title = request.form["title"]
         content = request.form["content"]
         quote = request.form["quote"]
         id = User.query.filter_by(username=session['username']).first().id
-        blog = Blog(content=content, title=title, blog_owner=id, blog_quote=quote, ref=generate_ref(12))
+        blog = Blog(content=content, title=title, blog_owner=id,
+                    blog_quote=quote, ref=generate_ref(12))
         db.session.add(blog)
         db.session.commit()
         return redirect(url_for("home"))
     return render_template("blog-sub.html")
+
 
 @app.route("/signup", methods=["POST", "GET"])
 def signup():
@@ -217,20 +290,25 @@ def signup():
         gender = request.form["gender"]
         password = hash(request.form["password"])
         v_password = hash(request.form["v_password"])
+        balance = get_con_balance(country)
+        referral_ref = generate_ref(12)
         user = User.query.filter_by(username=username).all()
         if user != []:
             flash("Username already exist", "danger")
         elif password != v_password:
             flash("Passwords do not match", "danger")
         else:
-            new_user = User(firstname=firstname, lastname=lastname, username=username, password=password, country=country, gender=gender)
+            if 'ref' in session:
+                User.query.filter_by(referral_ref=session['ref']).first(
+                ).balance += get_con_balance(User.query.filter_by(referral_ref=session['ref']).first().country)
+            new_user = User(balance=balance, firstname=firstname, lastname=lastname, username=username,
+                            password=password, country=country, gender=gender, referral_ref=referral_ref)
             db.session.add(new_user)
             db.session.commit()
             flash("Now Sign in", "info")
             return redirect(url_for("signin"))
         return render_template("signup.html")
     return render_template("signup.html")
-
 
 
 @app.route("/signin", methods=["POST", "GET"])
@@ -250,19 +328,24 @@ def signin():
         else:
             add_session(username)
             session['currency'] = get_currency(user.country)
+            if 'redirect' in session:
+                redirect_lk = session['redirect']
+                session.pop('redirect', None)
+                return redirect(redirect_lk)
             return redirect(url_for("home"))
         return render_template("signin.html")
     return render_template("signin.html")
 
+
 @app.route("/phishing")
 def phishing():
     return render_template("slips.html")
-    
+
+
 @app.route("/logout")
 def logout():
     delete_session()
     return redirect(url_for("signin"))
-
 
 
 @app.route("/slips")
@@ -282,16 +365,18 @@ def cart():
     return render_template('dashboard.html')
 
 
-
 @app.route("/submission", methods=["POST", "GET"])
 def submission():
     if not is_logged_in():
         flash("Please login first", "danger")
+        session['redirect'] = request.url
         return redirect(url_for("signin"))
     elif request.method == "POST":
         code = request.form["code"]
-        time = datetime.strptime(f"{datetime.now().date()} {request.form['time']}", "%Y-%m-%d %H:%M")
+        time = datetime.strptime(
+            f"{datetime.now().date()} {request.form['time']}", "%Y-%m-%d %H:%M")
         bookmaker = request.form["bookmaker"]
+        image = request.files['image']
         #price = request.form["price"]
         price = "0.00"
         description = request.form["description"]
@@ -304,17 +389,26 @@ def submission():
         if es is not None:
             flash("Oops! Bet code already exists", "danger")
             return redirect(url_for("submission"))
-        new_slip = Slip(bkl=link, code=code, odds=odds, time=time, bookmaker=bookmaker, price=price, description=description, slip_ref=slip_ref, slip_owner=id, matches=matches)
+        elif (image and allowed_file(image.filename)) == False:
+            flash("Oops! File type not supported.", "danger")
+            return redirect(url_for("submission"))
+        filename = secure_filename(
+            bookmaker.lower()+"_"+generate_ref(6)+".png")
+        image.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        new_slip = Slip(image=filename, bkl=link, code=code, odds=odds, time=time, bookmaker=bookmaker,
+                        price=price, description=description, slip_ref=slip_ref, slip_owner=id, matches=matches)
         db.session.add(new_slip)
         db.session.commit()
         return redirect(url_for("home"))
     return render_template('submission.html')
+
 
 @app.route("/detail",  methods=["POST", "GET"])
 def detail():
     ref = request.args.get('ref')
     if not is_logged_in():
         flash("Please login first", "danger")
+        session['redirect'] = request.url
         return redirect(url_for("signin"))
     elif ref == None:
         return redirect(url_for("home"))
@@ -329,6 +423,7 @@ def detail():
     addt_slips = Slip.query.filter_by(expired="Not expired").all()[0:3]
     return render_template("slip-detail.html", slip=slip, name=name, comments=comments, noc=len(comments.all()), addt_slips=addt_slips)
 
+
 @app.route("/blog-detail",  methods=["POST", "GET"])
 def blogDetail():
     ref = request.args.get('ref')
@@ -342,17 +437,18 @@ def blogDetail():
     #comments = SlipComment.query.filter_by(slip_id=slip.id)
     return render_template("blog-details.html", blog=blog, owner=owner)
 
+
 @app.route('/getcomments',  methods=["GET"])
 def getComments():
-    jsons = {'comments':[]}
+    jsons = {'comments': []}
     ref = request.args.get("ref")
     id = Slip.query.filter_by(slip_ref=ref).first().id
     comments = SlipComment.query.filter_by(slip_id=id).all()
     for comment in comments:
         x = {
-            'comment':comment.comment, 
+            'comment': comment.comment,
             'name': comment.commenter_name
-            }
+        }
         jsons['comments'].append(x)
     ress = jsonify(jsons)
     #ress.headers.add('Access-Control-Allow-Origin', "*")
@@ -360,22 +456,23 @@ def getComments():
     return res
 
 
-
 @app.route("/addcomments", methods=["POST"])
 def addCommentS():
+    req = request.get_json()
     if not is_logged_in():
         flash("Please login first", "danger")
+        session['redirect'] = "http://127.0.0.1:5000/detail?ref="+req['ref']
         return make_response(jsonify({'url': url_for('signin')}), 201)
     else:
-        req = request.get_json()
         comment = req['comment']
         ref = req['ref']
         slip = Slip.query.filter_by(slip_ref=ref).first()
         user = User.query.filter_by(username=session['username']).first()
         commenter_name = user.firstname + " " + user.lastname
-        newComment = SlipComment(comment=comment, commenter=user.id, slip_id=slip.id, commenter_name=commenter_name)
+        newComment = SlipComment(
+            comment=comment, commenter=user.id, slip_id=slip.id, commenter_name=commenter_name)
         db.session.add(newComment)
-        db.session.commit() 
+        db.session.commit()
         ress = jsonify({'message': 'OK'})
         #ress.headers.add('Access-Control-Allow-Origin', "*")
         res = make_response(ress, 200)
@@ -384,12 +481,13 @@ def addCommentS():
 
 @app.route("/addlike", methods=["POST"])
 def addLikes():
+    req = request.get_json()
     if not is_logged_in():
         flash("Please login first", "danger")
+        session['redirect'] = "http://127.0.0.1:5000/detail?ref="+req['ref']
         return make_response(jsonify({'url': url_for('signin')}), 201)
     else:
         flag = False
-        req = request.get_json()
         likeq = req['like']
         ref = req['ref']
         slip = Slip.query.filter_by(slip_ref=ref).first()
@@ -410,14 +508,75 @@ def addLikes():
         else:
             return res
 
-@app.route("/settings")
+
+@app.route("/settings", methods=['POST', 'GET'])
 def settings():
+    message = ""
     if not is_logged_in():
         flash("Please login first", "danger")
+        session['redirect'] = request.url
         return redirect(url_for("signin"))
+    elif request.method == "POST":
+        firstname = request.form['firstname']
+        lastname = request.form['lastname']
+        email = request.form['email']
+        bio = request.form['bio']
+        birthdate = request.form['birthdate']
+        phone = request.form['phone']
+
+        user = User.query.filter_by(username=session['username']).first()
+        user.firstname = firstname
+        user.lastname = lastname
+        user.email = email
+        user.DOB = birthdate
+        user.phone = phone
+        user.bio = bio
+
+        image = request.files['image']
+
+        if image.filename == '':
+            pass
+        elif (image and allowed_file(image.filename)) == False:
+            flash("File type not supported.", "danger")
+            return redirect(url_for("settings"))
+        else:
+            filename = secure_filename(user.username.lower(
+            )+"_"+generate_ref(20)+"."+image.filename.split(".")[-1].lower())
+            image.save(os.path.join("static/images/profiles/", filename))
+            user.image = filename
+
+        password = request.form['password']
+        new_password = request.form['new_password']
+        verify_new_password = request.form['verify_new_password']
+
+        if password == '' and (new_password != '' or verify_new_password != ''):
+            profile = User.query.filter_by(
+                username=session['username']).first()
+            message = "Please enter your current password before."
+        elif password != '' and (new_password == '' or verify_new_password == ''):
+            profile = User.query.filter_by(
+                username=session['username']).first()
+            message = "Please enter new password."
+        elif password != '' and new_password != '' or verify_new_password != '':
+            if hash(password) != user.password:
+                profile = User.query.filter_by(
+                    username=session['username']).first()
+                message = "You entered an incorrect password."
+            elif verify_new_password != new_password:
+                profile = User.query.filter_by(
+                    username=session['username']).first()
+                message = "New password does not match."
+            else:
+                user.password = hash(new_password)
+
+    db.session.commit()
+    if message != '':
+        flash(message, "danger")
     profile = User.query.filter_by(username=session['username']).first()
     return render_template("settings.html", profile=profile)
-#---------------------------------------------------
-#RUNNING APP
+
+
+# ---------------------------------------------------
+# RUNNING APP
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ static/src/Currency_converter/pycurrency.py
@@ -0,0 +1,49 @@
+import requests
+import json
+
+#import config
+
+
+#UNIVERSAL VARIABLES
+URL = f"https://v6.exchangerate-api.com/v6/0bad105e04f4e53dfc232b13/latest/USD"
+
+
+class Currency_Converter:
+	"""convert any convercy"""
+
+	def __init__(self):
+		self.data = requests.get(URL).json()
+		
+		#assign need info to varible
+		self.base_curruncy = self.data['base_code']
+		self.currencies = self.data['conversion_rates']
+
+
+	def convert(self, from_currency, to_currency, amount): 		
+		"""returns amout of needed currency"""
+
+		#convert from currency to base currency.
+		#base currency is USD
+		convert2_base = amount / self.currencies[from_currency]
+		
+		conversion_rate = self.currencies[to_currency]
+	  
+		# return only 2 decimal places 
+		amount_figure = round(convert2_base * self.currencies[to_currency], 2)
+		amount_figure = f"{to_currency} {amount_figure}"
+		
+		return 	{
+			"from_currency" : from_currency,
+			"to_currency" : to_currency,  
+			"input_amount" : amount,
+			"result" : amount_figure,
+			"conversion_rate" : conversion_rate,
+		}
+		
+		 
+
+
+#For testing
+if __name__ == "__main__":
+	converter = Currency_Converter()
+	print(converter.convert('USD','GHS',0.2))
